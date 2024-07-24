bc0c3efd6c5364d42eb43a75b6759fc1589b4e1e
CharlesLedwaba <poleloledwaba@gmail.com>
2024-07-24 17:16:49 +0200

    Initial commit

---
+++ back_end/Home_main.js
@@ -0,0 +1,26 @@
+// button element from html
+const cvBtn = document.querySelector('#DownloadBtn');
+const idBtn = document.querySelector('#DownloadIdBtn');
+const gradeBtn = document.querySelector('#DownloadGradeBtn');
+const transcriptBtn = document.querySelector('#DownloadTransBtn');
+
+
+// Onclick event to the CV button
+cvBtn.addEventListener('click', function() {
+    window.location.href = '../img/Resume.pdf';
+});
+
+// Onclick event to the ID button
+idBtn.addEventListener('click', function() {
+    window.location.href = '../img/Resume.pdf';
+});
+
+// Onclick event to the GRADE 12 RESULTS button
+gradeBtn.addEventListener('click', function() {
+    window.location.href = '../img/Resume.pdf';
+});
+
+// Onclick event to the TRANSCRIPT button
+transcriptBtn.addEventListener('click', function() {
+    window.location.href = '../img/Resume.pdf';
+});
